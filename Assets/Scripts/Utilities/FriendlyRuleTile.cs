722ca206429ed30ac55b680c0b66bb2e8b770e02
C-W-Z <chenweizhang3021@gmail.com>
2023-09-06 22:53:09 +0800

    update FriendlyRuleTile.cs

--- Assets/Scripts/Utilities/FriendlyRuleTile.cs
+++ Assets/Scripts/Utilities/FriendlyRuleTile.cs
@@ -4,36 +4,35 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu]
 public class FriendlyRuleTile : RuleTile<FriendlyRuleTile.Neighbor> {
     public bool customField;
-
     public TileBase[] friendTiles;
 
-    public class Neighbor : RuleTile.TilingRule.Neighbor {
+    public class Neighbor : RuleTile.TilingRule.Neighbor
+    {
         public const int Null = 3;
         public const int NotNull = 4;
         public const int ThisOrFriend = 5;
         public const int NotThisAndNotFriend = 6;
     }
 
-    public override bool RuleMatch(int neighbor, TileBase tile) {
-        switch (neighbor) {
-            case Neighbor.Null: return tile == null;
-            case Neighbor.NotNull: return tile != null;
-            case Neighbor.ThisOrFriend: return tile == this || HasFriendTile(tile);
-            case Neighbor.NotThisAndNotFriend: return tile != this && !HasFriendTile(tile);
-        }
-        return base.RuleMatch(neighbor, tile);
+    public override bool RuleMatch(int neighbor, TileBase tile)
+    {
+        return neighbor switch
+        {
+            Neighbor.Null                => tile == null,
+            Neighbor.NotNull             => tile != null,
+            Neighbor.ThisOrFriend        => tile == this || HasFriendTile(tile),
+            Neighbor.NotThisAndNotFriend => tile != this && !HasFriendTile(tile),
+            _ => base.RuleMatch(neighbor, tile),
+        };
     }
 
     bool HasFriendTile(TileBase tile)
-        {
-            if (tile == null)
-                return false;
- 
-            for (int i = 0; i < friendTiles.Length; i++)
-            {
-                if (friendTiles[i] == tile)
-                    return true;
-            }
+    {
+        if (tile == null)
             return false;
-        }
+        for (int i = 0; i < friendTiles.Length; i++)
+            if (friendTiles[i] == tile)
+                return true;
+        return false;
+    }
 }
